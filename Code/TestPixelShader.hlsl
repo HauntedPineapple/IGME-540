298046082c7168460459046ccdc0d41092b4b9a2
Audrey Main <meilong.main@gmail.com>
2023-02-27 21:47:28 -0500

    Diffuse working(?)

--- Code/TestPixelShader.hlsl
+++ Code/TestPixelShader.hlsl
@@ -16,18 +16,26 @@ float DiffuseBRDF(float3 normal, float3 dirToLight)
 
 float SpecularBRDF(float3 normal, float3 lightDir, float3 viewVector, float roughness)
 {
-
+    float specExponent = (1.0f - roughness) * MAX_SPECULAR_EXPONENT;
+    if (specExponent > 0.05)
+    {
+        float reflection = reflect(normalize(lightDir), normal);
+        float specular = saturate(dot(reflection, viewVector));
+        return pow(specular, specExponent);
+    }
+    
+    return 0.0f;
 }
 
 float4 main(VertexToPixel input) : SV_TARGET
-{    
+{
     input.normal = normalize(input.normal); // Must renormalize any interpolated vectors
     float3 directionToLight = normalize(-directionalLightA.direction);
-    //float3 viewVector = normalize(cameraPosition - input.worldPosition); // vector from surface to camera
+    float3 viewVector = normalize(cameraPosition - input.worldPosition); // vector from surface to camera
     
-    float3 totalLightColor = ambientColor;
+    float3 totalLightColor = ambientColor * colorTint;
     totalLightColor += DiffuseBRDF(input.normal, directionToLight) * directionalLightA.color;
     //totalLightColor += SpecularBRDF(input.normal, directionalLightA.direction, viewVector, roughness);
 
-    return float4((float3)colorTint * totalLightColor, 1);
+    return float4((float3)totalLightColor, 1);
 }
